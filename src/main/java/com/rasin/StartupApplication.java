ee11b5f0b97b7b65c798e9daf58e07b8335c3761
wangyu32 <285677770@qq.com>
2019-05-15 09:43:17 +0800

    init

--- src/main/java/com/rasin/StartupApplication.java
+++ src/main/java/com/rasin/StartupApplication.java
@@ -1,9 +1,18 @@
 package com.rasin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description
  * @Author wangyu
  * @Date 2019/5/14 21:26
  */
+@SpringBootApplication
 public class StartupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartupApplication.class, args);
+    }
+
 }
--- src/main/java/com/rasin/controller/HelloController.java
+++ src/main/java/com/rasin/controller/HelloController.java
@@ -1,9 +1,29 @@
 package com.rasin.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @Description
  * @Author wangyu
  * @Date 2019/5/14 21:22
  */
+@Slf4j
+@RestController
 public class HelloController {
+
+    private static DateFormat dateFormator = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @RequestMapping("/hello")
+    public String hello(){
+        Date d = new Date();
+        log.info("hello");
+        return dateFormator.format(d);
+    }
+
 }
