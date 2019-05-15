e1d0c050c774bd1c3fc6a2e80ec50c238731a260
wangyu32 <285677770@qq.com>
2019-05-15 09:42:07 +0800

    init

---
+++ src/main/java/com/rasin/StartupApplication.java
@@ -0,0 +1,9 @@
+package com.rasin;
+
+/**
+ * @Description
+ * @Author wangyu
+ * @Date 2019/5/14 21:26
+ */
+public class StartupApplication {
+}
---
+++ src/main/java/com/rasin/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.rasin.controller;
+
+/**
+ * @Description
+ * @Author wangyu
+ * @Date 2019/5/14 21:22
+ */
+public class HelloController {
+}
